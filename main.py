a85cefef9c05a52fa246c2d2c7ec34a3334aa2ea
imoudu13 <juboy1@yahoo.com>
2024-12-03 16:12:30 -0800

    :changed pathing

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ sample_2_new = " ../policy-sample-2-new.pdf"
 sample_2_expiring = "../policy-sample-2-expiring.pdf"
 my_sample_1 = "../my-sample-1.pdf"
 my_sample_2 = "../my-sample-2.pdf"
-reduced_1 = "../sample_policy_1.pdf"
-reduced_2 = "../sample_policy_2.pdf"
+reduced_1 = "sample_policy_1.pdf"
+reduced_2 = "sample_policy_2.pdf"
 # result = process_and_compare_pdfs(sample_1_new, sample_1_expiring)
 result1 = new.process_and_compare_pdfs(reduced_1, reduced_2)
 
